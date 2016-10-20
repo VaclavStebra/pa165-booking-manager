f952dc87ecf5db6adfcee3322bc1a2f473b65a3e
Vaclav Stebra <vstebra@gmail.com>
2016-10-20 21:31:38 +0200

    Add persistence annotations and javadoc for User class

--- BookingManager-persistence/src/main/java/cz/muni/fi/pa165/brown/entity/User.java
+++ BookingManager-persistence/src/main/java/cz/muni/fi/pa165/brown/entity/User.java
@@ -1,75 +1,179 @@
 package cz.muni.fi.pa165.brown.entity;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+
 /**
+ * Class for user Entity
  *
  * @author Vaclav Stebra
  */
+@Entity
 public class User {
 
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
+
+    @NotNull
     private String name;
+
+    @NotNull
     private String surname;
+
+    @NotNull
     private String address;
+
+    @NotNull
     private String email;
+
+    @NotNull
     private String password;
+
+    @NotNull
     private boolean admin;
 
     public User() {
     }
 
+    /**
+     *
+     * @return id of user
+     */
     public Long getId() {
         return id;
     }
 
+    /**
+     *
+     * @param id id of user
+     */
     public void setId(Long id) {
         this.id = id;
     }
 
+    /**
+     *
+     * @return name of user
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     *
+     * @param name name of user
+     */
     public void setName(String name) {
         this.name = name;
     }
 
+    /**
+     *
+     * @return surname of user
+     */
     public String getSurname() {
         return surname;
     }
 
+    /**
+     *
+     * @param surname surname of user
+     */
     public void setSurname(String surname) {
         this.surname = surname;
     }
 
+    /**
+     *
+     * @return address of user
+     */
     public String getAddress() {
         return address;
     }
 
+    /**
+     *
+     * @param address address of user
+     */
     public void setAddress(String address) {
         this.address = address;
     }
 
+    /**
+     *
+     * @return email of user
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     *
+     * @param email email of user
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     *
+     * @return hashed password of user
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     *
+     * @param password hashed password of user
+     */
     public void setPassword(String password) {
         this.password = password;
     }
 
+    /**
+     *
+     * @return true if user is System Administrator, false otherwise
+     */
     public boolean isAdmin() {
         return admin;
     }
 
+    /**
+     *
+     * @param admin flag whether user is System Administrator
+     */
     public void setAdmin(boolean admin) {
         this.admin = admin;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null) {
+            return false;
+        }
+        if ( ! (o instanceof User)) {
+            return false;
+        }
+        User user = (User) o;
+        if (email == null) {
+            if (user.email != null) {
+                return false;
+            }
+        }
+        return email.equals(user.email);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 + ((email == null) ? 0 : email.hashCode());
     }
 }
