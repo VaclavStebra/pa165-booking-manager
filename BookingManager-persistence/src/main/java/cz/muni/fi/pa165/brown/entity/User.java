af0adc25811d4c24eb2d2320b7211f6f83ac59ee
Vaclav Stebra <vstebra@gmail.com>
2016-10-20 17:28:44 +0200

    Add Template for User Entity

---
+++ BookingManager-persistence/src/main/java/cz/muni/fi/pa165/brown/entity/User.java
@@ -0,0 +1,75 @@
+package cz.muni.fi.pa165.brown.entity;
+
+/**
+ *
+ * @author Vaclav Stebra
+ */
+public class User {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private String address;
+    private String email;
+    private String password;
+    private boolean admin;
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
+    }
+}
