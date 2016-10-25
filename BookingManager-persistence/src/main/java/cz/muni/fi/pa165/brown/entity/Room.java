a9fba18710c2b87b2b61e2f583345c4b2d159bf7
Dominik Labuda <Dominik.Labuda@ysoft.com>
2016-10-25 17:20:43 +0200

    ManyToOne annotation added for Room.hotel field

--- BookingManager-persistence/src/main/java/cz/muni/fi/pa165/brown/entity/Room.java
+++ BookingManager-persistence/src/main/java/cz/muni/fi/pa165/brown/entity/Room.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
 
@@ -33,6 +34,7 @@ public class Room implements Serializable {
 
     /** Hotel in which the room is situated */
     @NotNull
+    @ManyToOne
     private Hotel hotel;
 
     /**
