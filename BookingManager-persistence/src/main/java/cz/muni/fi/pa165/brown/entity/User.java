38b6e268c0b6a94a3e77bde79fb10dde639626c1
Vaclav Stebra <vstebra@gmail.com>
2016-10-25 16:16:43 +0200

    Removed unused imports, refactor createHotel in HotelDaoTest, add column nullable=false and unique constraints in User entity

--- BookingManager-persistence/src/main/java/cz/muni/fi/pa165/brown/entity/User.java
+++ BookingManager-persistence/src/main/java/cz/muni/fi/pa165/brown/entity/User.java
@@ -17,21 +17,27 @@ public class User {
     private Long id;
 
     @NotNull
+    @Column(nullable=false)
     private String name;
 
     @NotNull
+    @Column(nullable=false)
     private String surname;
 
     @NotNull
+    @Column(nullable=false)
     private String address;
 
     @NotNull
+    @Column(nullable=false, unique=true)
     private String email;
 
     @NotNull
+    @Column(nullable=false)
     private String password;
 
     @NotNull
+    @Column(nullable=false)
     private boolean admin;
 
     public User() {
