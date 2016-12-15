33c1ec3fc77c433b3c52ef674d37996ff4bc8172
hagaram <of.hagara@gmail.com>
2016-12-15 21:46:51 +0100

    rest implemented

---
+++ BookingManager-rest/src/main/java/cz/muni/fi/pa165/brown/AllowOriginInterceptor.java
@@ -0,0 +1,21 @@
+package cz.muni.fi.pa165.brown;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by michal hagara
+ */
+
+
+public class AllowOriginInterceptor extends HandlerInterceptorAdapter {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        return true;
+    }
+}
---
+++ BookingManager-rest/src/main/java/cz/muni/fi/pa165/brown/controllers/GlobalExceptionController.java
@@ -0,0 +1,27 @@
+package cz.muni.fi.pa165.brown.controllers;
+
+import cz.muni.fi.pa165.brown.ApiError;
+import cz.muni.fi.pa165.brown.exceptions.ResourceAlreadyExistingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.Arrays;
+
+/**
+ * @author michal hagara
+ */
+@ControllerAdvice
+public class GlobalExceptionController {
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+    @ResponseBody
+    ApiError handleException(ResourceAlreadyExistingException ex) {
+        ApiError apiError = new ApiError();
+        apiError.setErrors(Arrays.asList("the requested resource already exists"));
+        return apiError;
+    }
+}
